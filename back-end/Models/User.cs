46dc3820b11e2f3882488d16fbd91dad7a1e2724
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-11 14:05:55 -0300

    primeiro commit

---
+++ back-end/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace back_end.Models;
+
+public class User
+{
+    // Propriedades da classe User
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string? Email { get; set; }
+    public DateOnly Birthday { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
