ec9c321c5c637ca81ec59baf019226033978ffd1
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-19 11:12:52 -0300

    Dto pro metodo de listar usuarios

---
+++ back-end/DTOs/UserCreatedAtDto.cs
@@ -0,0 +1,10 @@
+﻿namespace back_end.DTOs;
+
+public class UserCreatedAtDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public DateTime Birthday { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
