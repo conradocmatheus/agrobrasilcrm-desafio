f49ce297009f92685119dc2b8b6b678495da2538
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-19 15:00:15 -0300

    metodos basicos de Productrepositories  getAll, getById, Add, Delete

---
+++ back-end/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using back_end.Models;
+
+namespace back_end.Repositories;
+
+public interface IProductRepository
+{
+    Task AddProductAsync(Product product);
+    Task<List<Product>> GetAllProductsAsync();
+    Task<Product?> GetProductByIdAsync(Guid id);
+    Task<Product?> DeleteProductByIdAsync(Guid id);
+}
---
+++ back-end/Repositories/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using back_end.Data;
+using back_end.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace back_end.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly AppDbContext _context;
+
+    public ProductRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task AddProductAsync(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<Product>> GetAllProductsAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    public async Task<Product?> GetProductByIdAsync(Guid id)
+    {
+        return await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<Product?> DeleteProductByIdAsync(Guid id)
+    {
+        var existingProduct = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (existingProduct == null)
+        {
+            return null;
+        }
+
+        _context.Products.Remove(existingProduct);
+        await _context.SaveChangesAsync();
+        return existingProduct;
+    }
+}
