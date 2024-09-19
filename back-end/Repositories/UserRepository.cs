724a984ef7fc3ebbe4e3c5bc02e075231b6edfd5
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-19 10:27:20 -0300

    GetUsersByCreatedAtAsync no repositorio

--- back-end/Repositories/UserRepository.cs
+++ back-end/Repositories/UserRepository.cs
@@ -1,5 +1,7 @@
 ﻿using back_end.Data;
+using back_end.DTOs;
 using back_end.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace back_end.Repositories;
 
@@ -12,10 +14,14 @@ public class UserRepository : IUserRepository
         _context = context;
     }
 
-
     public async Task AddUserAsync(User user)
     {
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<User>> GetUsersByCreatedAtAsync(Guid id)
+    {
+        return await _context.Users.ToListAsync();
     }
 }
