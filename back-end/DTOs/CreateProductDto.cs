386eda29af61e89491dd8f813f617c0d575eb64f
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-19 15:26:31 -0300

    Annotations no nome na Dto

---
+++ back-end/DTOs/CreateProductDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace back_end.DTOs;
+
+public class CreateProductDto
+{
+    [MinLength(1, ErrorMessage = "Nome não pode ser vazio")]
+    [MaxLength(200, ErrorMessage = "Não ultrapasse 200 caracteres")]
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+}
--- back-end/DTOs/ProductDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace back_end.DTOs;
-
-public class ProductDto
-{
-    public string Name { get; set; }
-    public int Quantity { get; set; }
-    public double Price { get; set; }
-}
