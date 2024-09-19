0631e5f5ce0feaed8649e1371f8f5453fb827308
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-19 15:55:17 -0300

    metodos basicos de produtos ainda tenho q fazer o controller e o update produtos, e dps testar

---
+++ back-end/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using back_end.DTOs;
+
+namespace back_end.Services;
+
+public interface IProductService
+{
+    Task<CreateProductDto> CreateProductAsync(CreateProductDto createProductDto);
+    Task<List<ProductDto>> GetAllProducts();
+    Task<ProductDto?> GetProductByIdAsync(Guid id);
+    Task<ProductDto?> DeleteProductByIdAsync(Guid id);
+}
