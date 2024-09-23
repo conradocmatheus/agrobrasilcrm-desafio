c6068c92e031c4787cf4868e6e8bed3bf22eec3e
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-23 14:21:16 -0300

    Comentários nos metodos

--- back-end/Repositories/UserRepositories/UserRepository.cs
+++ back-end/Repositories/UserRepositories/UserRepository.cs
@@ -13,6 +13,7 @@ public class UserRepository : IUserRepository
         _context = context;
     }
 
+    // Criar usuário no banco
     public async Task<User> CreateUserAsync(User user)
     {
         // Gera o id tipo GUID e as datas de criação e atualização em (UTC)
@@ -27,47 +28,8 @@ public class UserRepository : IUserRepository
         // Retorna o usuário criado, com id e datas préviamente preenchidas
         return user;
     }
-
-    public async Task<List<User>> GetUsersByCreatedAtAsync()
-    {
-        // Retorna o usuário pelo ID ou nulo se não for encontrado
-        return await _context.Users
-            .OrderBy(user => user.CreatedAt)
-            .AsNoTracking() // Melhora a performance
-            .ToListAsync();
-    }
-
-    public async Task<User?> GetUserByIdAsync(Guid id)
-    {
-        // Retorna o usuário pelo ID ou nulo se não for encontrado
-        return await _context.Users
-            .AsNoTracking() // Melhora a performance
-            .FirstOrDefaultAsync(x => x.Id == id);
-    }
-
-    public async Task<User?> DeleteUserByIdAsync(Guid id)
-    {
-        try
-        {
-            // Pega o usuário com o id fornecido ou null se não encontrar
-            var existingUser = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
-
-            if (existingUser == null)
-            {
-                return null;
-            }
-
-            // Remove o usuário do banco, salva e retorna o próprio usuário removido
-            _context.Users.Remove(existingUser);
-            await _context.SaveChangesAsync();
-            return existingUser;
-        }
-        catch (Exception e)
-        {
-            throw new InvalidOperationException("Não foi possível deletar o usuário.", e);
-        }
-    }
-
+    
+    // Atualizar usuário no banco por ID
     public async Task<User?> UpdateUserAsync(User user, Guid id)
     {
         try
@@ -93,5 +55,48 @@ public class UserRepository : IUserRepository
             throw new InvalidOperationException($"Não foi possível atualizar," +
                                                 $" usuário com ID {id} não encontrado.", e);
         }
+    }
+    
+    // Deletar usuário no banco por ID
+    public async Task<User?> DeleteUserByIdAsync(Guid id)
+    {
+        try
+        {
+            // Pega o usuário com o id fornecido ou null se não encontrar
+            var existingUser = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingUser == null)
+            {
+                return null;
+            }
+
+            // Remove o usuário do banco, salva e retorna o próprio usuário removido
+            _context.Users.Remove(existingUser);
+            await _context.SaveChangesAsync();
+            return existingUser;
+        }
+        catch (Exception e)
+        {
+            throw new InvalidOperationException("Não foi possível deletar o usuário.", e);
+        }
+    }
+
+    // Listar usuários de acordo com a data de criação
+    public async Task<List<User>> GetUsersByCreatedAtAsync()
+    {
+        // Retorna o usuário pelo ID ou nulo se não for encontrado
+        return await _context.Users
+            .OrderBy(user => user.CreatedAt)
+            .AsNoTracking() // Melhora a performance
+            .ToListAsync();
+    }
+
+    // Encontra usuário por ID
+    public async Task<User?> GetUserByIdAsync(Guid id)
+    {
+        // Retorna o usuário pelo ID ou nulo se não for encontrado
+        return await _context.Users
+            .AsNoTracking() // Melhora a performance
+            .FirstOrDefaultAsync(x => x.Id == id);
     }
 }
