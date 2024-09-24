f0d9fb4f1ca21f7ae6388f079bca8d1becdbeb15
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-24 10:04:43 -0300

    nova classe intermediaria movementProduct

---
+++ back-end/Models/MovementProduct.cs
@@ -0,0 +1,14 @@
+﻿namespace back_end.Models;
+
+public class MovementProduct
+{
+        // Propriedades da classe intermediária MovementProduct
+        public Guid MovementId { get; set; }
+        public Movement Movement { get; set; }
+
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+
+        // Você pode adicionar propriedades adicionais, se necessário
+        public int Quantity { get; set; }
+}
