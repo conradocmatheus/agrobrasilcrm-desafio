8b98e700c8f23d457d5be77995e32b673432a7b7
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-25 21:37:13 -0300

    ao inves das annotations coloquei as verificacoes no service

--- back-end/DTOs/ProductDTOs/CreateProductDto.cs
+++ back-end/DTOs/ProductDTOs/CreateProductDto.cs
@@ -1,11 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace back_end.DTOs;
+namespace back_end.DTOs.ProductDTOs;
 
 public class CreateProductDto
 {
-    [MinLength(1, ErrorMessage = "Nome não pode ser vazio")]
-    [MaxLength(200, ErrorMessage = "Não ultrapasse 200 caracteres")]
     public string Name { get; set; }
     public double Price { get; set; }
 }
