92771d7754a75bf123725ebb483826c56bfc4096
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-25 22:11:00 -0300

    adicionei validacoes na dto

--- back-end/DTOs/ProductDTOs/CreateProductDto.cs
+++ back-end/DTOs/ProductDTOs/CreateProductDto.cs
@@ -1,9 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using back_end.CustomActionFilters;
 
 namespace back_end.DTOs.ProductDTOs;
 
 public class CreateProductDto
 {
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [NoNumbers]
     public string Name { get; set; }
+    
+    [Required(ErrorMessage = "Preco e obrigatorio.")]
     public double Price { get; set; }
 }
--- back-end/DTOs/UserDTOs/CreateUserDto.cs
+++ back-end/DTOs/UserDTOs/CreateUserDto.cs
@@ -1,11 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using back_end.Filters;
+using back_end.CustomActionFilters;
 
 namespace back_end.DTOs.UserDTOs;
 
 public class CreateUserDto
 {
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [NoNumbers]
     public required string Name { get; set; }
+    
+    [Required(ErrorMessage = "O email é obrigatório.")]
+    [EmailAddress(ErrorMessage = "Endereço de email inválido.")]
     public required string Email { get; set; }
+    
+    [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
+    [DataType(DataType.Date)]
     public required DateTime Birthday { get; set; }
 }
